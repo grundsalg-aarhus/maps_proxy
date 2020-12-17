8a8cd04c14e9bf9851ee5dfd1f2b04ef08faf4f5
Jesper Kristensen <cableman@linuxdev.dk>
2020-12-17 15:24:53 +0100

    GRUND-486: Force updated all plugin security issues in packages

--- upgrade.sh
+++ upgrade.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-# Install main modules
-rm -rf node_modules
-npm install --${1:-production}
-
 # Install plugin dependencies.
 for folder in plugins/*; do
   if [ -d $folder ]; then
     cd $folder
-    npm-check-updates -u
-    rm -rf node_modules
-    npm install --${1:-production}
+    npm audit fix
     cd ../..
   fi
 done
